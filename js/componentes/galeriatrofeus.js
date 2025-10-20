1b340326423e8bb35feeb9a692f9c69510a3391a
Pedro Favela <pedro.pereira.favela@hotmail.com>
2025-10-19 22:47:55 -0300

    Feat: Criação do componente da galeria de trofeus no arquivo JavaScript

---
+++ js/componentes/galeriatrofeus.js
@@ -0,0 +1,73 @@
+export default function galeriatrofeus(){
+    const content = `
+        <section class="section">
+            <h1>Galeria de troféus</h1>
+        </section>
+        <section class="section dark-bg">
+            <div class="container">
+                <h2>Austrália - 2025</h2>
+                <div class="content-grid">
+                    <div class="image-content">
+                        <img src="https://anholeto.com.br/wp-content/uploads/2024/02/2296-pedido-7379.jpg" alt="">
+                    </div>
+                    <div class="image-content">
+                        <img src="https://anholeto.com.br/wp-content/uploads/2024/02/2296-pedido-7379.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section">
+            <div class="container">
+                <h2>Capão redondo - 2024</h2>
+                <div class="content-grid">
+                    <div class="image-content">
+                        <img src="https://anholeto.com.br/wp-content/uploads/2024/02/2296-pedido-7379.jpg" alt="">
+                    </div>
+                    <div class="image-content">
+                        <img src="https://anholeto.com.br/wp-content/uploads/2024/02/2296-pedido-7379.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section dark-bg">
+            <div class="container">
+                <h2>Guarulhos - 2020</h2>
+                <div class="content-grid">
+                    <div class="image-content">
+                        <img src="https://anholeto.com.br/wp-content/uploads/2024/02/2296-pedido-7379.jpg" alt="">
+                    </div>
+                    <div class="image-content">
+                        <img src="https://anholeto.com.br/wp-content/uploads/2024/02/2296-pedido-7379.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section">
+           <div class="container">
+                <h2>ESTRAGOS UNIDOS - 2020</h2>
+                <div class="content-grid">
+                    <div class="image-content">
+                        <img src="https://anholeto.com.br/wp-content/uploads/2024/02/2296-pedido-7379.jpg" alt="">
+                    </div>
+                    <div class="image-content">
+                        <img src="https://anholeto.com.br/wp-content/uploads/2024/02/2296-pedido-7379.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section dark-bg">
+            <div class="container">
+                <h2>BRASÍLIA - 2019</h2>
+                <div class="content-grid">
+                    <div class="image-content">
+                        <img src="https://anholeto.com.br/wp-content/uploads/2024/02/2296-pedido-7379.jpg" alt="">
+                    </div>
+                    <div class="image-content">
+                        <img src="https://anholeto.com.br/wp-content/uploads/2024/02/2296-pedido-7379.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+    `
+    return content
+}
