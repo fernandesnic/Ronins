664df9259574205bd7992a1efaede7b5069bec47
Nicolas <nicolasbafernandes@gmail.com>
2025-11-08 12:42:06 -0300

    Implement user update and delete routes in private.js

--- backend/routes/private.js
+++ backend/routes/private.js
@@ -22,4 +22,32 @@ router.get("/list", async (req, res) => {
   }
 });
 
+router.put("/update/:id", async (req, res) => {
+  try { 
+    let { id } = req.params; 
+    
+
+    const updatedUser = await prisma.User.update({
+      where: { id: id },
+      data: req.body,
+    });
+    res.status(200).json({ message: "User updated successfully", user: updatedUser });
+  } catch (error) {
+    console.error("Error in /update route:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+})
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+ const { id } = req.params;
+ const deletedUser = await prisma.User.delete({
+  where: { id }
+ })
+ res.status(200).json({message: "User deleted successfully", user: deletedUser})
+} catch (error) {
+  console.error("Error in /delete route:", error);
+res.status(500).json({ error: "Internal Server Error" });
+}
+})
 export default router;
