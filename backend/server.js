04e28f683f45e3853641f2cacc40d8f6620bc7af
Nicolas <nicolasbafernandes@gmail.com>
2025-11-09 18:38:56 -0300

    Feat: Add CORS configuration and backend URL to frontend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,24 @@ import privateRoutes from "./routes/private.js"
 import auth from "./middlewares/auth.js";
 import cors from "cors";
 
+const allowedOrigins = [
+  'https://ronins-omega.vercel.app/', // 1. Sua URL de produção
+  'http://localhost:5500',                  // 2. Sua URL de dev (ajuste a porta se for diferente)
+  'http://127.0.0.1:5500'                   // 3. (Opcional) Às vezes o Live Server usa 127.0.0.1
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Se a origem da requisição ESTÁ na lista (ou se for 'undefined' - ex: Postman)
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true); // Permite
+    } else {
+      callback(new Error('Não permitido pelo CORS')); // Bloqueia
+    }
+  }
+};
+app.use(cors(corsOptions));
+
 const app = express();
 const PORT = 3000;
 app.use(express.json());
---
+++ frontend/js/url.js
@@ -0,0 +1,1 @@
+const BACKEND_URL = "https://ronins.onrender.com/"
